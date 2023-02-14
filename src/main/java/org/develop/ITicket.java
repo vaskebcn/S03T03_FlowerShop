0aebc8883995e10ff03f9770453bea22e8b3c166
carlajuani <carla90_@hotmail.es>
2023-02-14 10:05:09 +0100

    modificacio metodes i interface

--- src/main/java/org/develop/ITicket.java
+++ src/main/java/org/develop/ITicket.java
@@ -3,11 +3,6 @@ package org.develop;
 import java.util.HashMap;
 
 public interface ITicket {
-    static int idCounter = 1;
-    int id = 0;
-    public HashMap<Integer,TicketLine> ticketLines = new HashMap<Integer,TicketLine>();
-    double totalPrice = 0;
-
-    public void addProduct(Product product);
+    public void addTicketLine(IProduct product);
     public double getTotalPrice();
 }
--- src/main/java/org/develop/Ticket.java
+++ src/main/java/org/develop/Ticket.java
@@ -4,26 +4,34 @@ import java.util.HashMap;
 
 public class Ticket implements ITicket{
     static int idCounter = 1;
-    int id = 0;
+    final int ID;
     HashMap<Integer,TicketLine> ticketLines = new HashMap<Integer,TicketLine>();
     double totalPrice = 0;
 
     public Ticket () {
-        id += idCounter;
+        this.ID = idCounter;
         idCounter++;
+    }
+    public int getID() {
+        return ID;
+    }
+    public HashMap<Integer,TicketLine> getTicketLines() {
+        return ticketLines;
     }
 
     @Override
-    public void addProduct(Product product) {
-        if (ticket.containsValue(product)) {
+    public void addTicketLine(IProduct product) {
+        TicketLine ticketLine = new TicketLine(product);
+        if (ticketLines.containsValue(ticketLine)) {
                 product.add1ToQuantity();
         } else {
-            ticket.put(ticket.size(), product);
+            ticketLines.put(ticketLines.size(), ticketLine);
         }
     }
 
     @Override
     public double getTotalPrice() {
+
         return 0;
     }
 }
