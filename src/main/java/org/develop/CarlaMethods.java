f407e7e508f88b11c349091b731ecd327522009a
carlajuani <carla90_@hotmail.es>
2023-02-20 20:40:29 +0100

    milos i microcanvis carla

--- src/main/java/org/develop/CarlaMethods.java
+++ src/main/java/org/develop/CarlaMethods.java
@@ -5,23 +5,25 @@ import java.util.HashMap;
 
 public class CarlaMethods {
 
-    public static void printCatalog(String storeName) {
-        //JSON STUFF aqui s'agafa el json de la store guardat al stores.txt i es guarda de nou com a store¿
-        (json)store.showStock();
+    public static void printCatalog(String storeFile) {
+        //JSON STUFF agafar productsPepitaStore.txt, i imprimir forEach line(products)
     }
 
     //no tinc clar que no lhagi fet la Montse, sino primer copiar el seu format per addProduct a la inversa
-    public static void removeProduct() {
+    public static HashMap<String,IStore> removeProductFromStore(HashMap<String,IStore> storesList) {
         String storeName = Input.scanningForString("Please indicate the products store's name");
         //JSON STUFF aqui s'agafa el json de la store guardat al stores.txt i es guarda de nou com a store¿
         Store store = null;
         String ref = Input.scanningForString("Please indicate product's reference");
-        store.getStoreStock().remove(ref);
-        System.out.println("The product has been successfully removed from the store's stock");
+        if (store.getStoreStock().containsKey(ref)) {
+            store.getStoreStock().remove(ref);
+            System.out.println("The product has been successfully removed from the store's stock");
+        }
         //JSON STUFF aqui es passa again la store a object json i es fa un overwrite d'aquesta al stores.txt
+        retur
     }
 
-    public static void showAllPreviousSales(String storeName) {
+    public static void printHistorySales(String storeName) {
         //JSON STUFF aqui es mira quin registre de tickets es daquesta store i s'agafa com a json del LaPepitaTickets.txt. Igual sha de canviar el parametre d'entrada
         (json)salesHistory.values().forEach(System.out::println);
     }
